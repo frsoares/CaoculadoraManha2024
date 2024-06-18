5a0525e1de4662772703ca4d684470f64887c72f
stnrfzz <stonerfuzz00@gmail.com>
2024-06-18 09:27:35 -0300

    refacts file

--- Caoculadora/Caoculadora/Porte.swift
+++ Caoculadora/Caoculadora/Porte.swift
@@ -14,21 +14,17 @@ enum Porte: String, CaseIterable {
 
     func calcularIdade(deAnos anosCaninos: Int,
                       eMeses mesesCaninos: Int) -> Int {
-        // o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
-        // multiplicador:
-        //   * pequeno: 6
-        //   * médio: 7
-        //   * grande: 8
+
         
         let multiplicador: Int
         
         switch self {
         case .pequeno:
-            multiplicador = 6
+            multiplicador = 10
         case .medio:
-            multiplicador = 7
+            multiplicador = 20
         case .grande:
-            multiplicador = 8
+            multiplicador = 30
         }
 
         let result = anosCaninos * multiplicador + mesesCaninos * multiplicador / 12
